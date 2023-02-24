b34f6ed8d0dfc5a24764ae430227f8b972845cd8
Горбулёв Сергей <gorbulevsv@yandex.ru>
2023-02-25 00:22:56 +0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,62 @@
+<script>
+  import ButtonGoUp from "./ButtonGoUp.svelte";
+
+  let date = new Date().toISOString().slice(0, 10);
+  $: src = `http://www.patriarchia.ru/bu/${date}/print.html`;
+</script>
+
+<div id="top" class="d-flex flex-column" style="min-height: 100vh;">
+  <div
+    class="sticky-bottom d-flex gap-1 align-items-center w-100 bg-light text-dark p-2"
+  >
+    <div class="d-flex flex-grow-1">
+      <div class="input-group">
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <span
+          class="btn btn-light text-dark bg-transparent border"
+          style="cursor:default;"
+          on:click={() => {
+            date = new Date().toISOString().slice(0, 10);
+          }}>Дата:</span
+        >
+        <input
+          type="date"
+          class="form-control bg-opacity-10"
+          title="Выберите дату"
+          bind:value={date}
+        />
+        <button
+          class="btn btn-light text-dark border"
+          title="Предыдущий день"
+          on:click={() => {
+            let dateNext = new Date(date);
+            dateNext.setDate(dateNext.getDate() - 1);
+            date = dateNext.toISOString().slice(0, 10);
+          }}><i class="fa-solid fa-arrow-left" /></button
+        >
+        <button
+          class="btn btn-light text-dark border"
+          title="Следующий день"
+          on:click={() => {
+            let dateNext = new Date(date);
+            dateNext.setDate(dateNext.getDate() + 1);
+            date = dateNext.toISOString().slice(0, 10);
+          }}><i class="fa-solid fa-arrow-right" /></button
+        >
+      </div>
+    </div>
+
+    <div class="d-flex">
+      <div class="input-group">
+        <button
+          class="btn btn-light text-dark border"
+          on:click={() => (date = new Date().toISOString().slice(0, 10))}
+          >Сегодня</button
+        >
+      </div>
+    </div>
+  </div>
+
+  <!-- svelte-ignore a11y-missing-attribute -->
+  <iframe class="flex-grow-1" {src} frameborder="0" />
+</div>
---
+++ src/ButtonGoUp.svelte
@@ -0,0 +1,11 @@
+<script>
+  let y = 0;
+</script>
+
+<svelte:window bind:scrollY={y} />
+
+<button
+  id="btn-back-to-top"
+  class="position-fixed bottom-0 end-0 m-2 btn btn-light bg-opacity-10 text-dark"
+  on:click={() => (y = 0)}><i class="fa-solid fa-arrow-up-long" /></button
+>
